08b50623c6d68db31c7841d8c89bb53a1a1e7bcd
Viktor <mr.viktor.tkachev@gmail.com>
2022-07-23 20:50:36 +0300

    Made a math solution

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,25 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = {};
+  if (a == 0) {
+    return folse;
+  }
+  let D = Math.pow(b, 2) - 4 * a * c;
+  arr = D;
+
+  if (D < 0) {
+    arr = [];
+  }
+  if (D == 0) {
+    arr = [-b / (2 * a)];
+  } else if (D > 0) {
+    let tmp = [];
+    tmp.push((-b + Math.sqrt(D)) / (2 * a));
+    tmp.push((-b - Math.sqrt(D)) / (2 * a));
+    arr = tmp;
+  }
+
   return arr; // array
 }
 
