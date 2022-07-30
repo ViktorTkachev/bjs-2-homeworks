668bb77632b73b29483f5d4394c64fe4bee646c4
Viktor <mr.viktor.tkachev@gmail.com>
2022-07-30 23:02:41 +0300

    make 3 tasks to Function topic

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,54 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  sum = 0;
+  min = arr[0];
+  max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  avg = +(sum / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = func(arrOfArr[0]);
+  for (let i = 1; i < arrOfArr.length; i++) {
+    if (max < func(arrOfArr[i])) {
+      max = func(arrOfArr[i]);
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max, diff;
+  min = arr[0];
+  max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  diff = Math.abs(max - min);
+  return diff;
 }
