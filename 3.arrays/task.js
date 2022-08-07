b0bf0cfff9045629dff63d3634cfa8dd693df461
Viktor <mr.viktor.tkachev@gmail.com>
2022-08-07 15:20:30 +0300

    make 2 tasks to arrays topic

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1.length != arr2.length || arr1[i] != arr2[i]) {
+      result = false;
+    } else {
+      result = true;
+    }
+  }
+  return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(e => e >= 0).filter(e => e % 3 === 0).map((item) => item * 10)
+  return resultArr;
 }
