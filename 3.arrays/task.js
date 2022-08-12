039e2acbc1c61fe3760d82aeb6325fcdc90cf28f
Viktor <mr.viktor.tkachev@gmail.com>
2022-08-12 21:47:49 +0300

    corrected functions

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,16 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1.length != arr2.length || arr1[i] != arr2[i]) {
-      result = false;
-    } else {
-      result = true;
-    }
-  }
-  return result;
+  return arr1.length == arr2.length && arr1.every((element,index) => element == arr2[index])
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter(e => e >= 0).filter(e => e % 3 === 0).map((item) => item * 10)
-  return resultArr;
+  return arr.filter(e => e >= 0).filter(e => e % 3 === 0).map((item) => item * 10)
 }
