adab9d0cce5c98b18645f866ec73d47930479fe6
Viktor <mr.viktor.tkachev@gmail.com>
2022-11-07 17:07:08 +0300

    Homework was completed. But Jasmine test was lose. Chekc please this

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Buzz", "female", 35);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+  let countMarks = this.marks.length;
+  let averageMarks = this.marks.reduce((acc, item) => acc += item, 0);
+  return averageMarks/countMarks;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+}
