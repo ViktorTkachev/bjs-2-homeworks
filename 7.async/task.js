340eee7b6dbba54b81fdffb325d544ad4879dbac
Viktor <mr.viktor.tkachev@gmail.com>
2022-12-09 17:25:00 +0800

    made decorators homework

--- 7.async/task.js
+++ 7.async/task.js
@@ -11,16 +11,13 @@ class AlarmClock {
     if(this.alarmCollection.find((alarm) => alarm.id === id)){
       console.error('Будильник с таким id уже существует!');
     } else {
-      // this.alarmCollection.push({time, callback, id});
-      this.alarmCollection = [...this.alarmCollection, {time, callback, id}];
+      this.alarmCollection.push({time, callback, id});
     }
   }
 
   removeClock(id) {
-    let findAlarm = this.alarmCollection.find((alarm) => alarm.id === id);
-    if (findAlarm) {
-      this.alarmCollection = this.alarmCollection.filter((alarm) => alarm.id ==! id);
-    }
+    let findAlarm = this.alarmCollection.every((alarm) => alarm.id === id);
+    this.alarmCollection = this.alarmCollection.filter((alarm) => alarm.id ==! id);
     return findAlarm;
   }
 
@@ -29,14 +26,14 @@ class AlarmClock {
   }
 
   start() {
-    function checkClock(getAlarm, time) {
-      if (getAlarm.time === time) {
+    const checkClock = (getAlarm) => {
+      if (getAlarm.time === this.getCurrentFormattedTime()) {
         getAlarm.callback();
       }
     }
 
     if (this.timerId === null) {
-      this.timerId = setInterval(() => this.alarmCollection.forEach((alarm) => checkClock(alarm, this.getCurrentFormattedTime())));
+      this.timerId = setInterval(() => this.alarmCollection.forEach((alarm) => checkClock(alarm)));
     }
   }
 
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,33 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  return (...args) => {
+    const hash = args.join(',');
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+    let result = func(...args);
+    cache.push({hash: hash, value: result});
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = true;
+  wrapper.allCount = 0;
+  wrapper.count = 0;
+  function wrapper (...args) {
+    clearTimeout(timeoutId);
+    wrapper.allCount++;
+    if(timeoutId) {
+      timeoutId = setTimeout(() => {func(...args), wrapper.count++}, delay);
+    }
+  }
+  return wrapper;
 }
